9966a2f40a585a98eab73a279e11670ee2f07eef
Matheus Andre <matheusandr2@gmail.com>
2024-07-17 15:26:32 -0300

    Subindo implementacao e modelagem

---
+++ Agenda.Domain/Agenda/Calendario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agenda.Domain.Base;
+
+namespace Agenda.Domain.Agenda
+{
+    public  class Calendario : Entity<Guid>
+    {
+        public string Nome { get; set; }
+        private readonly List<Compromisso> _compromisso = new List<Compromisso>();
+
+        public void AdicionarCompromisso(Compromisso compromisso)
+        {
+            //Logica
+            _compromisso.Add(compromisso);
+        }
+
+        public void RemoverCompromisso(Compromisso compromisso)
+        {
+            //Logica
+            _compromisso.Add(compromisso);
+        }
+    }
+}
+    }
+}
